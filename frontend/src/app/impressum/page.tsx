f64109fb92b9c460735ece7af914d2387b55655d
Mac Daria <dariaschmidt0510@gmail.com>
2025-09-10 10:55:59 +0200

    Add Impressum, Add Privat Policy

---
+++ frontend/src/app/impressum/page.tsx
@@ -0,0 +1,26 @@
+export default function Impressum() {
+  return (
+    <div>
+      <h1>Impressum</h1>
+
+      <p>
+        Daria Schmidt
+        <br />
+        Am Lax 31
+        <br />
+        36251 Bad Hersfeld
+      </p>
+
+      <h2>Kontakt</h2>
+      <p>
+        Telefon: +49 152 25827496
+        <br />
+        E-Mail: kontakt@dariaschmidt.de
+      </p>
+
+      <p>
+        Quelle: <a href="https://www.e-recht24.de">eRecht24</a>
+      </p>
+    </div>
+  );
+}
